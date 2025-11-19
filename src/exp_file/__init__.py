922085fa3457b3dfbe90307fbea7261dbb3a5340
joshrmcdaniel <80354972+joshrmcdaniel@users.noreply.github.com>
2025-11-19 10:04:53 -0600

    stubs

--- src/exp_file/__init__.py
+++ src/exp_file/__init__.py
@@ -1,5 +1,8 @@
 from .decode import decode_exp_file
 
-_all__ = [
+__all__ = [
     "decode_exp_file",
 ]
+
+if __name__ == "__main__":
+    raise NotImplementedError("CLI is not implemented. Please use the 'exp_file' package functions.")
---
+++ src/exp_file/cli.py
@@ -0,0 +1,3 @@
+"""
+TODO: implement CLI functionality
+"""
